83d270c6190c3ceac13820d4124b58dde185b76f
Divyanshu <divyanshusingh4755@gmail.com>
2021-08-05 15:37:30 +0530

    Added Header with css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
 import HomePage from "./pages/homepage/HomePage"
 import Shop from './pages/shop/Shop';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route path="/" exact={true} component={HomePage} />
         <Route path="/shop" exact={true} component={Shop} />
