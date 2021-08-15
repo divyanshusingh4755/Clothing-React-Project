0be44a416c4ec46a4c3691c311a995df4c25d4ab
Divyanshu <divyanshusingh4755@gmail.com>
2021-08-15 21:00:01 +0530

    Added remove functionality in cart

--- src/components/checkoutItem/CheckoutItem.jsx
+++ src/components/checkoutItem/CheckoutItem.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./checkoutItem.scss";
+import { clearItemFromCart } from "../../redux/cart/cartActions";
 
-const CheckoutItem = ({ item }) => {
+const CheckoutItem = ({ item, clearItem }) => {
     const { imageUrl, name, quantity, price } = item
     return (
         <div className="checkout-item">
@@ -11,9 +13,13 @@ const CheckoutItem = ({ item }) => {
             <span className="name">{name}</span>
             <span className="quantity">{quantity}</span>
             <span className="price">{price}</span>
-            <div className="remove">&#10005;</div>
+            <div className="remove" onClick={() => clearItem(item)}>&#10005;</div>
         </div>
     )
 }
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
