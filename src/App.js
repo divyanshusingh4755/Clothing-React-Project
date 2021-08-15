80cdb34e11d944e1c3912e001b950113030506e1
Divyanshu <divyanshusingh4755@gmail.com>
2021-08-15 10:13:07 +0530

    added user action types and redirect in component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import HomePage from "./pages/homepage/HomePage"
 import Shop from './pages/shop/Shop';
-import signInSignUp from './pages/signInSignUp/signInSignUp';
+import SignInSignUp from './pages/signInSignUp/signInSignUp';
 import { setCurrentUser } from './redux/user/userAction';
 
 class App extends Component {
@@ -28,7 +28,7 @@ class App extends Component {
           })
         })
       } else {
-        setCurrentUser({ userAuth })
+        setCurrentUser(userAuth)
       }
     })
   }
@@ -44,16 +44,25 @@ class App extends Component {
         <Switch>
           <Route path="/" exact={true} component={HomePage} />
           <Route path="/shop" component={Shop} />
-          <Route path="/sign-in" component={signInSignUp} />
+          <Route exact path="/sign-in" render={() =>
+            this.props.currentUser ?
+              <Redirect to='/' />
+              :
+              <SignInSignUp />
+          } />
         </Switch>
       </Router>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
